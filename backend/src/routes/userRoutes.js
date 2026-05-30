a86e9f9557626a008be90411d357a3df51aa8e75
Eminence <pteminence@gmail.com>
2026-05-29 19:39:00 -0500

    fix: defined multer upload middleware in userRoutes

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
 const userController = require('../controllers/userController');
 const { verifyToken, checkRole } = require('../middleware/auth');
 
